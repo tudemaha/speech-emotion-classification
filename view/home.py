08f3967f2aac107c17695a2d9d8f3604db824bcf
tudemaha <gede_mahardika88@yahoo.co.id>
2023-07-01 23:39:31 +0800

    add html view

--- view/home.py
+++ view/home.py
@@ -1,10 +1,13 @@
-import streamlit as st
 from st_pages import Page, show_pages
+import streamlit.components.v1 as components
 
 from view.config import get_config
 
 def start_home():
     get_config("Home")
+
+    render_page = open("view/index.html")
+    components.html(render_page.read(), height = 1700)
 
     show_pages(
         [
